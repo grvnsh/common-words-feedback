c1f6e07d23030ff89bba88d88eff15554bb16a5a
Gurvansh Gill <gurvanshsgill@gmail.com>
2024-11-06 22:43:42 -0500

    Update common_words_feedback.rs

--- common_words_feedback.rs
+++ common_words_feedback.rs
@@ -1,7 +1,6 @@
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::{BufRead, BufReader};
-
 fn main() {
     // Open the file
     let file = File::open("ielts_feedback.txt").expect("Failed to open file");
